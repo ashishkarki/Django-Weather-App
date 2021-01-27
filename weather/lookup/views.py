85bfdd023ee73afa43f5e7b60dfe2c8aea365d53
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-01-27 16:54:26 +0545

    first step to making real api calls

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    import weather_app_secrets
+
+    api_url = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=02452&date=2021-01-27&distance=25&API_KEY=" + weather_app_secrets.API_KEY
+    api_request = requests.get(api_url)
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error"
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
