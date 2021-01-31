d6a7ea428e2258c605e34cbd857a3cf930755557
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-01-31 19:35:27 +0545

    Bootstrapi-fying about page with some good info

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -8,12 +8,14 @@ def home(request):
     import lookup.lookup_constants
     from datetime import date
 
-    zipCode = '02453' # default zip: Boston area, MA, USA
+    zipCode = '02453'  # default zip: Boston area, MA, USA
     if request.method == "POST":
         zipCode = request.POST.get('zipCode', '02453')
 
     date_today = date.today()
-    api_url = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode="+ zipCode + "&date="+ str(date_today) + "&distance=25&API_KEY=" + weather_app_secrets.API_KEY
+    api_url = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=" + \
+        zipCode + "&date=" + \
+        str(date_today) + "&distance=25&API_KEY=" + weather_app_secrets.API_KEY
     api_request = requests.get(api_url)
 
     try:
@@ -29,25 +31,25 @@ def home(request):
     category_description = ""
 
     if CATEGORY_NAME == "Good":
-        category_description = "(0 to 50) Air quality issatisfactory, and air pollution poses little or no risk"
+        category_description = "(0 to 50) Air quality is satisfactory, and air pollution poses little or no risk"
         category_color = "good"
     elif CATEGORY_NAME == "Moderate":
-        category_description = "(51 to 100) Air quality isacceptable. However, there may be a risk for somepeople, particthose who are unusually sensitive to airpoll"
+        category_description = "(51 to 100) Air quality is acceptable. However, there may be a risk for somepeople, particthose who are unusually sensitive to airpollution."
         category_color = "moderate"
     elif CATEGORY_NAME == "Unhealthy for Sensitive Groups":
-        category_description = "(101 to 150) Members ofsensitive groups may experience health effects. Thegeneral public ilikely to be aff"
+        category_description = "(101 to 150) Members ofsensitive groups may experience health effects"
         category_color = "usg"
     elif CATEGORY_NAME == "Unhealthy":
-        category_description = "(151 to 200) Some members ofthe general public may experience health effects"
+        category_description = "(151 to 200) Some members of the general public may experience health effects."
         category_color = "unhealthy"
     elif CATEGORY_NAME == "Very Unhealthy":
-        category_description = "(201 to 300) Health alert: Therisk of health effects is increased for eve"
+        category_description = "(201 to 300) Health alert: The risk of health effects is increased."
         category_color = "veryunhealthy"
     elif CATEGORY_NAME == "Hazardous":
-        category_description = "(301 and higher) Health warningof emergency conditions: everyone is more likely to beaffect"
+        category_description = "(301 and higher) Health warning of emergency conditions: everyone is more likely to be affected."
         category_color = "hazardous"
     else:
-        category_description = "The lookup didn't returnanything. Please try again."
+        category_description = "The lookup didn't return anything. Please try again."
         category_color = "blue-grey darken-1"
     ###
 
